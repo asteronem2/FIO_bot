3b8886abb18a3e31551befb8dda4465e5f1df907
asteronem2 <welesikn@gmail.com>
2024-11-10 15:19:29 +0300

    5 commit

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN: str = os.environ.get('BOT_TOKEN')
-ADMIN_ID = 892097042
+ADMIN_ID = 1016944643
 
 if not BOT_TOKEN:
     raise Exception('You need to write environment variable BOT_TOKEN in ".env" file')
